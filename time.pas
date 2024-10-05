28f341eaab792f69104f1eddf32f6f9734995cef
Alexander Simonov <aleksandrsimonov9622@gmail.com>
2024-10-05 10:35:53 +0300

    New file

---
+++ time.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var y := ReadInteger('Введите год: ');
+  if ((y mod 4) = 0) and ((y mod 100) = (y mod 400)) then Print('Введеный год - високосный')
+  else Print('Введенный год не високосный')
+end.
