239bd976e61443a41a452f79f275b87633e75deb
Alexander Simonov <aleksandrsimonov9622@gmail.com>
2024-10-05 10:28:33 +0300

    First changes

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Привет, мир!');
+  var time := ReadInteger('Введите время: ');
+  Assert((time <= 23) and (time >= 0));
+  if (time >= 4) and (time <= 10) then Print('Доброе утро, мир!')
+  else if (time >= 11) and (time <= 16) then Print('Добрый день, мир!')
+  else if (time >= 17) and (time <= 22) then Print('Добрый вечер, мир!')
+  else 
+    Print('Доброй ночи, мир!')
 end.
