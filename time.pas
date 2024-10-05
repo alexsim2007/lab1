a03cad810848f6b0411a1a4fcd88d2130db6bbfe
Alexander Simonov <aleksandrsimonov9622@gmail.com>
2024-10-05 10:49:24 +0300

    Changes in time.pas and new file - timer.pas

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿begin
   var y := ReadInteger('Введите год: ');
-  if ((y mod 4) = 0) and ((y mod 100) = (y mod 400)) then Print('Введеный год - високосный')
-  else Print('Введенный год не високосный')
+  if ((y mod 4) = 0) and ((y mod 100) = (y mod 400)) then Print('В этом году 366 дней')
+  else Print('В этом году 365 дней')
 end.
---
+++ timer.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var h := ReadInteger('Введите кол-во часов: ');
+  var m := ReadInteger('Введите кол-во минут: ');
+  var s := ReadInteger('Введите кол-во секунд: ');
+  Assert((h <= 24) and (h >= 0));
+  Assert((m >= 0) and (m <= 59));
+  Assert((s >= 0) and (s <= 59));
+  Print($'Суммарное кол-во секунд: {(h * 3600) + (m * 60) + s}');
+end.
